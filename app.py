38927e3abf225e43b912d791f7ee9c0390380b59
Pavel Golovanov <publichead@yandex.ru>
2020-09-12 22:30:39 +0300

    serialization/deserialization with marshmallow / flaskapispec

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity
 from config import Config
+from apispec.ext.marshmallow import MarshmallowPlugin
+from apispec import APISpec
+from flask_apispec.extension import FlaskApiSpec
+from schemas import VideoSchema, UserSchema, AuthSchema
+from flask_apispec import use_kwargs, marshal_with
 
 app = Flask(__name__)
 app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
@@ -21,6 +26,19 @@ Base.query = session.query_property()
 
 jwt = JWTManager(app)
 
+docs = FlaskApiSpec()
+docs.init_app(app)
+
+app.config.update({
+    "APISPEC_SPEC": APISpec(
+        title='videoblog',
+        version='v1',
+        openapi_version='2.0',
+        plugins=[MarshmallowPlugin()]
+    ),
+    'APISPEC_SWAGGER_URL': '/swagger/'
+})
+
 from models import *
 
 Base.metadata.create_all(bind=engine)
@@ -28,91 +46,102 @@ Base.metadata.create_all(bind=engine)
 
 @app.route("/tutorials", methods=["GET"])
 @jwt_required
+@marshal_with(VideoSchema(many=True))
 def get_list():
-    user_id = get_jwt_identity()
-    videos = Video.query.filter(Video.user_id == user_id).all()
-    serialized = []
-    for video in videos:
-        serialized.append(
-            {
-                "id": video.id,
-                "user_id": video.user_id,
-                "name": video.name,
-                "description": video.description,
-            }
-        )
-    return jsonify(serialized)
+    try:
+        user_id = get_jwt_identity()
+        videos = Video.query.filter(Video.user_id == user_id).all()
+    except Exception as e:
+        return {'message': str(e)}, 400
+    return videos
 
 
 @app.route("/tutorials", methods=["POST"])
 @jwt_required
-def update_list():
-    user_id = get_jwt_identity()
-    new_one = Video(user_id=user_id, **request.json)
-    session.add(new_one)
-    session.commit()
-    serialized = {
-        "id": new_one.id,
-        "user_id": new_one.user_id,
-        "name": new_one.name,
-        "description": new_one.description,
-    }
-    return jsonify(serialized)
+@use_kwargs(VideoSchema)
+@marshal_with(VideoSchema)
+def update_list(**kwargs):
+    try:
+        user_id = get_jwt_identity()
+        new_one = Video(user_id=user_id, **kwargs)
+        session.add(new_one)
+        session.commit()
+    except Exception as e:
+        return {'message': str(e)}, 400
+    return new_one
 
 
 @app.route("/tutorials/<int:tutorial_id>", methods=["PUT"])
 @jwt_required
-def update_tutorial(tutorial_id):
-    user_id = get_jwt_identity()
-    item = Video.query.filter(Video.id == tutorial_id, Video.user_id == user_id).first()
-    params = request.json
-    if not item:
-        return {"message": "No tutorials with this id"}, 400
-    for key, value in params.items():
-        setattr(item, key, value)
-    session.commit()
-    serialized = {
-        "id": item.id,
-        "user_id": item.user_id,
-        "name": item.name,
-        "description": item.description,
-    }
-    return serialized
+@use_kwargs(VideoSchema)
+@marshal_with(VideoSchema)
+def update_tutorial(tutorial_id, **kwargs):
+    try:
+        user_id = get_jwt_identity()
+        item = Video.query.filter(Video.id == tutorial_id, Video.user_id == user_id).first()
+        if not item:
+            return {"message": "No tutorials with this id"}, 400
+        for key, value in kwargs.items():
+            setattr(item, key, value)
+        session.commit()
+    except Exception as e:
+        return {'message': str(e)}, 400
+    return item
 
 
 @app.route("/tutorials/<int:tutorial_id>", methods=["DELETE"])
 @jwt_required
+@marshal_with(VideoSchema)
 def delete_tutorial(tutorial_id):
-    user_id = get_jwt_identity()
-    item = Video.query.filter(Video.id == tutorial_id, Video.user_id == user_id).first()
-    if not item:
-        return {"message": "No tutorials with this id"}, 400
-    session.delete(item)
-    session.commit()
+    try:
+        user_id = get_jwt_identity()
+        item = Video.query.filter(Video.id == tutorial_id, Video.user_id == user_id).first()
+        if not item:
+            return {"message": "No tutorials with this id"}, 400
+        session.delete(item)
+        session.commit()
+    except Exception as e:
+        return {'message': str(e)}, 400
     return "", 204
 
 
 @app.route("/register", methods=["POST"])
-def register():
-    params = request.json
-    user = User(**params)
-    session.add(user)
-    session.commit()
-    token = user.get_token()
+@use_kwargs(UserSchema)
+@marshal_with(AuthSchema)
+def register(**kwargs):
+    try:
+        user = User(**kwargs)
+        session.add(user)
+        session.commit()
+        token = user.get_token()
+    except Exception as e:
+        return {'message': str(e)}, 400
     return {"access_token": token}
 
 
 @app.route("/login", methods=["POST"])
-def login():
-    params = request.json
-    user = User.authenticate(**params)
-    token = user.get_token()
+@use_kwargs(UserSchema(only=('email', 'password')))
+@marshal_with(AuthSchema)
+def login(**kwargs):
+    try:
+        user = User.authenticate(**kwargs)
+        token = user.get_token()
+    except Exception as e:
+        return {'message': str(e)}, 400
     return {"access_token": token}
 
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     session.remove()
+
+
+docs.register(get_list)
+# docs.register(update_list)
+# docs.register(update_tutorial)
+docs.register(delete_tutorial)
+#docs.register(register)
+#docs.register(login)
 
 
 if __name__ == "__main__":
---
+++ schemas.py
@@ -0,0 +1,21 @@
+from marshmallow import Schema, validate, fields
+
+
+class VideoSchema(Schema):
+    id = fields.Integer(dump_only=True)
+    user_id = fields.Integer(dump_only=True)
+    name = fields.String(required=True, validate=validate.Length(max=250))
+    description = fields.String(required=True, validate=validate.Length(max=500))
+    message = fields.String(dump_only=True)
+
+
+class UserSchema(Schema):
+    name = fields.String(required=True, validate=validate.Length(max=250))
+    email = fields.String(required=True, validate=validate.Length(max=250))
+    password = fields.String(required=True, validate=validate.Length(max=100), load_only=True)
+    videos = fields.Nested(VideoSchema, many=True, dump_only=True)
+
+
+class AuthSchema(Schema):
+    access_token = fields.String(dump_only=True)
+    message = fields.String(dump_only=True)
